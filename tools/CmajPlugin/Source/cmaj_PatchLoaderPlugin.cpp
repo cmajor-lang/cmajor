070ab31a50c1a347b3954e822d0dcd52c7b6662c
Cesare Ferrari <cesare.ferrari@gmail.com>
2022-11-23 12:25:54 +0000

    Added CmajPlugin tool

---
+++ tools/CmajPlugin/Source/cmaj_PatchLoaderPlugin.cpp
@@ -0,0 +1,23 @@
+//  //
+//  //     ,ad888ba,                                88
+//  //    d8"'    "8b
+//  //   d8            88,dPba,,adPba,   ,adPPYba,  88      The Cmajor Language
+//  //   88            88P'  "88"   "8a        '88  88
+//  //   Y8,           88     88     88  ,adPPPP88  88      (c)2022 Sound Stacks Ltd
+//  //    Y8a.   .a8P  88     88     88  88,   ,88  88      https://cmajor.dev
+//  //     '"Y888Y"'   88     88     88  '"8bbP"Y8  88
+//  //                                             ,88
+//  //                                           888P"
+
+#include <JuceHeader.h>
+
+#include "../../../include/cmajor/helpers/cmaj_JUCEPlugin.h"
+
+
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    auto patch = std::make_unique<cmaj::Patch> (false);
+    patch->createEngine = +[] { return cmaj::Engine::create(); };
+
+    return new cmaj::plugin::JITLoaderPlugin (std::move (patch));
+}
