cmajor-lang/cmajor

commit 6fda81a58d4f85e4c70324fca6ea62f98b00ed3b

JUCE Plugin wrapper, maybe working as AUv3?

include/cmajor/helpers/cmaj_JUCEPlugin.h
@@ -62,7 +62,7 @@ public:
         {
             patch->stopPlayback    = [this] { suspendProcessing (true); };
             patch->startPlayback   = [this] { suspendProcessing (false); };
-            patch->patchChanged    = [this] { handlePatchChange(); };
+            patch->patchChanged    = [this] { juce::MessageManager::callAsync ([this] { handlePatchChange(); }); };
             patch->statusChanged   = [this] (const auto& s) { setStatusMessage (s.statusMessage, s.messageList.hasErrors()); };
 
             patch->handleOutputEvent = [this] (uint64_t frame, std::string_view endpointID, const choc::value::ValueView& v)
@@ -995,6 +995,8 @@ private:
         //==============================================================================
        #if JUCE_MAC
         using NativeUIBase = juce::NSViewComponent;
+       #elif JUCE_IOS
+        using NativeUIBase = juce::UIViewComponent;
        #elif JUCE_WINDOWS
         using NativeUIBase = juce::HWNDComponent;
        #else
@@ -1011,7 +1013,7 @@ private:
             {
                 setSize ((int) patchView->width, (int) patchView->height);
 
-               #if JUCE_MAC
+               #if JUCE_MAC || JUCE_IOS
                 setView (patchView->getWebView().getViewHandle());
                #elif JUCE_WINDOWS
                 setHWND (patchView->getWebView().getViewHandle());
@@ -1020,7 +1022,7 @@ private:
 
             ~PatchGUIHolder()
             {
-               #if JUCE_MAC
+               #if JUCE_MAC || JUCE_IOS
                 setView ({});
                #elif JUCE_WINDOWS
                 setHWND ({});
@@ -1407,7 +1409,7 @@ public:
        #if JUCE_WINDOWS
         path.add (juce::File::getSpecialLocation (juce::File::globalApplicationsDirectory)
                     .getChildFile ("Common Files\\Cmajor"));
-       #elif JUCE_MAC
+       #elif JUCE_MAC || JUCE_IOS
         path.add (juce::File ("/Library/Audio/Plug-Ins/Cmajor"));
         path.add (juce::File ("~/Library/Audio/Plug-Ins/Cmajor"));
        #endif

commit 785c6ec6e12f567fceaf1a78a82e6fd820dc435c

Plugin: fix missing parameters in generated plugin
* preliminary AUv3 work in 6fda81a58d4f85e4c70324fca6ea62f98b00ed3b
  regressed the behaviour of the generated plugins. for example, a
  generated VST3 plugin would have no parameters because the parameters
  were setup too late for the JUCE wrapper to report to the DAW
* it seems AUv3 plugins are sometimes initialised on a thread other than
  the message thread, at least for the auval tool, and we hit some juce
  asserts etc about that for tests where the editor has been
  instantiated. for now, keep this workaround of deferring to the
  message loop in place, but conditionally. there is certainly a better
  fix in the long term.

include/cmajor/helpers/cmaj_JUCEPlugin.h
@@ -25,6 +25,8 @@
 #include "cmaj_GeneratedCppEngine.h"
 #include "../../choc/memory/choc_xxHash.h"
 
+#include <utility>
+
 #if JUCE_LINUX
  #define Font FontX  // Gotta love these C headers with global symbol clashes.. sigh..
  #define Time TimeX
@@ -62,7 +64,18 @@ public:
         {
             patch->stopPlayback    = [this] { suspendProcessing (true); };
             patch->startPlayback   = [this] { suspendProcessing (false); };
-            patch->patchChanged    = [this] { juce::MessageManager::callAsync ([this] { handlePatchChange(); }); };
+            patch->patchChanged    = [this]
+            {
+                const auto executeOrDeferToMessageThread = [] (auto&& fn) -> void
+                {
+                    if (juce::MessageManager::getInstance()->isThisTheMessageThread())
+                        return fn();
+
+                    juce::MessageManager::callAsync (std::forward<decltype (fn)> (fn));
+                };
+
+                executeOrDeferToMessageThread ([this] { handlePatchChange(); });
+            };
             patch->statusChanged   = [this] (const auto& s) { setStatusMessage (s.statusMessage, s.messageList.hasErrors()); };
 
             patch->handleOutputEvent = [this] (uint64_t frame, std::string_view endpointID, const choc::value::ValueView& v)