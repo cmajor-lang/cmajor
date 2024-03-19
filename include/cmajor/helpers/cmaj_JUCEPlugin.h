f970464c36903616c900aecfb665ba2f4c99423f
Mark Claydon <mark@markclaydon.com>
2024-03-19 12:55:50 +0000

    Fixed not found error by adding juce namespace to AudioProcessorListener

--- include/cmajor/helpers/cmaj_JUCEPlugin.h
+++ include/cmajor/helpers/cmaj_JUCEPlugin.h
@@ -449,7 +449,7 @@ private:
 
     void handlePatchChange()
     {
-        auto changes = AudioProcessorListener::ChangeDetails::getDefaultFlags();
+        auto changes = juce::AudioProcessorListener::ChangeDetails::getDefaultFlags();
 
         auto newLatency = (int) patch->getFramesLatency();
 
