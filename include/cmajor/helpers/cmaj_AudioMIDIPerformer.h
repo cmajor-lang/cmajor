9a2875b8e85af298d138d50c333fa89c6b341d44
Cesare Ferrari <cesare.ferrari@gmail.com>
2024-03-28 20:40:19 +0000

    Fix for windows build

--- include/cmajor/helpers/cmaj_AudioMIDIPerformer.h
+++ include/cmajor/helpers/cmaj_AudioMIDIPerformer.h
@@ -409,7 +409,7 @@ void AudioMIDIPerformer::Builder::addOutputCopyFunction (EndpointHandle endpoint
             add (dest.getChannel (c.dest), source.getChannel (c.source));
     });
 
-    if (std::is_same<SampleType, float>::value && numChannelsInEndpoint == 1 && channelsToAddTo.empty())
+    if (numChannelsInEndpoint == 1 && channelsToAddTo.empty() && (std::is_same<SampleType, float>::value))
     {
         if (channelsToOverwrite.size() == 1)
         {
