883a5a2be1282b1025fbbd909b5bd75a9fc3d13d
Julian Storer <julianstorer@gmail.com>
2024-04-01 10:38:04 +0100

    Added logging and some fixes to the webview patch worker class

--- include/cmajor/helpers/cmaj_PatchWorker_WebView.h
+++ include/cmajor/helpers/cmaj_PatchWorker_WebView.h
@@ -46,6 +46,30 @@ inline void enableWebViewPatchWorker (Patch& p)
             opts.fetchResource = [this] (const std::string& path) { return fetchResource (path); };
             webview = std::make_unique<choc::ui::WebView> (opts);
 
+            webview->bind ("_cmaj_console_log", [] (const choc::value::ValueView& args) mutable -> choc::value::Value
+            {
+                if (args.isArray() && args.size() != 0)
+                {
+                    auto level = choc::javascript::LoggingLevel::log;
+
+                    if (args.size() > 1)
+                    {
+                        switch (args[1].getWithDefault<int> (1))
+                        {
+                            case 0: level = choc::javascript::LoggingLevel::log; break;
+                            case 1: level = choc::javascript::LoggingLevel::info; break;
+                            case 2: level = choc::javascript::LoggingLevel::warn; break;
+                            case 3: level = choc::javascript::LoggingLevel::error; break;
+                            case 4: level = choc::javascript::LoggingLevel::debug; break;
+                        }
+                    }
+
+                    consoleLog (args[0], level);
+                }
+
+                return {};
+            });
+
             webview->bind ("cmaj_sendMessageToServer", [send = std::move (sendMessageToPatch)] (const choc::value::ValueView& args) -> choc::value::Value
             {
                 if (args.isArray() && args.size() != 0)
@@ -89,6 +113,16 @@ inline void enableWebViewPatchWorker (Patch& p)
             webview->navigate ({});
         }
 
+        static void consoleLog (const choc::value::ValueView& content, choc::javascript::LoggingLevel level)
+        {
+            auto message = content.isString() ? content.toString() : choc::json::toString (content);
+
+            if (level == choc::javascript::LoggingLevel::debug)
+                std::cerr << message << std::endl;
+            else
+                std::cout << message << std::endl;
+        }
+
         void sendMessage (const std::string& msg, std::function<void(const std::string&)> reportError) override
         {
             webview->evaluateJavascript ("window.currentView?.deliverMessageFromServer(" + msg + ");",
@@ -115,6 +149,11 @@ inline void enableWebViewPatchWorker (Patch& p)
 
         static std::string getHTML (const PatchManifest& manifest)
         {
+            auto patchWorkerPath = manifest.patchWorker;
+
+            if (! choc::text::startsWith (patchWorkerPath, "/"))
+                patchWorkerPath = "/" + patchWorkerPath;
+
             return choc::text::replace (R"(
 <!DOCTYPE html>
 <html></html>
@@ -122,7 +161,6 @@ inline void enableWebViewPatchWorker (Patch& p)
 <script type="module">
 
 import { PatchConnection } from "./cmaj_api/cmaj-patch-connection.js"
-import runWorker from WORKER_MODULE
 
 class WorkerPatchConnection  extends PatchConnection
 {
@@ -154,10 +192,18 @@ class WorkerPatchConnection  extends PatchConnection
     }
 }
 
+window.console.log   =  function() { for (let a of arguments) _cmaj_console_log (a, 0); };
+window.console.info  =  function() { for (let a of arguments) _cmaj_console_log (a, 1); };
+window.console.warn  =  function() { for (let a of arguments) _cmaj_console_log (a, 2); };
+window.console.error =  function() { for (let a of arguments) _cmaj_console_log (a, 3); };
+window.console.debug =  function() { for (let a of arguments) _cmaj_console_log (a, 4); };
+
 try
 {
     const connection = new WorkerPatchConnection();
-    await runWorker (connection);
+
+    const workerModule = await import (WORKER_MODULE);
+    await workerModule.default (connection);
 }
 catch (e)
 {
@@ -167,7 +213,7 @@ catch (e)
 </script>
 )",
             "MANIFEST", choc::json::toString (manifest.manifest),
-            "WORKER_MODULE", choc::json::getEscapedQuotedString (manifest.patchWorker));
+            "WORKER_MODULE", choc::json::getEscapedQuotedString (patchWorkerPath));
         }
 
         Patch& patch;
