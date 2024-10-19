71c4492823446c61d14a0f934d94bdb5c03bcec1
Stéphane Letz <letz@grame.fr>
2024-10-19 16:28:21 +0200

    Updated with libfaust-2.75.15.

--- examples/patches/FaustFM/transform/faustTransformer.js
+++ examples/patches/FaustFM/transform/faustTransformer.js
@@ -46,9 +46,9 @@ class CmajorCompiler {
     compile(dsp_name, dsp_content, argv) {
         try {
             // Customize the compilation arguments
-            argv = argv + "-lang cmajor-hybrid -cn " + dsp_name + " -o foo.cmajor";
+            argv = argv + `-lang cmajor-hybrid -cn ${dsp_name} -o ${dsp_name}.cmajor`;
             const res = this.fCompiler.generateAuxFiles(dsp_name, dsp_content, argv);
-            return (res) ? this.fFS.readFile("foo.cmajor", { encoding: "utf8" }) : null;
+            return (res) ? this.fFS.readFile(`${dsp_name}.cmajor`, { encoding: "utf8" }) : null;
         }
         catch (e) {
             // Enhanced error handling to provide more detailed feedback
